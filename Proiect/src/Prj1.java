cc8b5f6ad60561e444f78da2b40c540fe68db373
oanaianosiu <56689765+oanaianosiu@users.noreply.github.com>
2019-10-19 07:21:50 +0300

    proiectv2

---
+++ Proiect/src/Prj1.java
@@ -0,0 +1,8 @@
+class Prj1 {
+	public static void main(String args[]) {
+		int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31,30, 31 };
+		String months[] ={“Ianuarie”,”Februarie”,”Martie”,”Aprilie”,”Mai”,”Iunie”,“Iulie”,”August”,”Septembrie”,”Octombrie”,”Noiembrie”,“Decembrie”};
+		for(int i=0; i<=month_day.lenght-1; i++);
+			System.out.println(months[i]+" "+month_day[i]+" ");
+	}
+}
---
+++ Proiect/src/meniu/Restaurant.java
@@ -0,0 +1,5 @@
+package meniu;
+
+public class Restaurant {
+
+}
